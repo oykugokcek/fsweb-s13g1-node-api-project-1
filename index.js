b698db2e9a64efb4b4904a63387052d690ae4747
Oyku <oykgokcek@gmail.com>
2023-02-21 17:01:41 +0300

    Revert "put request not checked"
    This reverts commit 3bc758c0aaa93f780aa9dcc7cb9917d9d94ba106.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const server = require("./api/server");
+const server = require('./api/server');
 
 const port = 9000;
 
-server.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
 // START YOUR SERVER HERE
